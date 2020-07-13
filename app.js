2795cdb659418023bb59488d88dd32b95b842c42
Khalil Mccall <khalilmccall@Khalils-MacBook-Pro.local>
2020-07-13 01:44:10 -0400

    new Commit

--- app.js
+++ app.js
@@ -1,62 +1,12 @@
 
 
 var button = document.querySelector('.button');
-
-var city = document.querySelector('.city');
-var state = document.querySelector('.state');
-var form =document.querySelector('.form');
 var weatherPage = document.querySelector('.weatherPage');
-var nameText = document.querySelector('.nameText');
+var form =document.querySelector('.form');
+var city;
+var state;
+var url;
 
-/*weatherPage.classList.add('bottom');
-form.classList.toggle("top");*/
-
-
-/*
-var weatherData = {
-"coord": {
-"lon": -81.66,
-"lat": 30.33
-},
-"weather": [
-{
-"id": 211,
-"main": "Thunderstorm",
-"description": "thunderstorm",
-"icon": "11d"
-}
-],
-"base": "stations",
-"main": {
-"temp": 89.46,
-"feels_like": 91.33,
-"temp_min": 87.8,
-"temp_max": 93.99,
-"pressure": 1013,
-"humidity": 58
-},
-"visibility": 16093,
-"wind": {
-"speed": 12.75,
-"deg": 270
-},
-"clouds": {
-"all": 75
-},
-"dt": 1594234586,
-"sys": {
-"type": 1,
-"id": 5093,
-"country": "US",
-"sunrise": 1594204290,
-"sunset": 1594254702
-},
-"timezone": -14400,
-"id": 4160021,
-"name": "Jacksonville",
-"cod": 200
-};
-*/
 
 var conditions = {
 
@@ -84,32 +34,30 @@ var conditions = {
 
 
 
-
-
-
-
-
-
-
-
-
-button.addEventListener('click',function(){
-
+function tog(){
 weatherPage.classList.toggle('bottom');
 form.classList.toggle("top");
+}
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+city.value+','+state.value+'&units=imperial&appid=1997240d660c8ec4c84f3c60d31c6079')
-.then(res => {
-	if(res.ok){
-		console.log("SUCCESS!");
-		return res.json()
-	}else{
-		console.log("NO SUCCESS");
-	}
-})
-.then(data =>{
-	console.log(data)
-	var weatherData = data;
+function reset(){
+document.querySelector('.date').textContent = "";
+document.querySelector('.temp').textContent = "";
+document.querySelector('.weatherPage').style.background = "#24252A";
+document.querySelector('.weatherPage').style.backgroundPosition = "center";
+document.querySelector('.saying').textContent = "";
+document.querySelector(".setImg").src = "";
+
+}
+
+
+function weatherMe(weatherData){
+		
+	
+		console.log(weatherData);
+		console.log("this is the data!");
+
+	
+	console.log(weatherData["name"]);
 		document.querySelector('.nameText').textContent = weatherData["name"];
 
 	var date = new Date();
@@ -119,7 +67,7 @@ var cityMinutes = weatherData["timezone"] / 60;
 date.setMinutes(date.getMinutes() + offSet + cityMinutes);
 console.log(date);
 
-var days = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"];
+var days = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
 
 var hours = date.getHours();
@@ -144,11 +92,55 @@ var background = "url('" +weatherData["weather"][0]["main"]+".jpg')";
 document.querySelector(".saying").textContent = conditions[weatherData["weather"][0]["main"]]["saying"];
 
 document.querySelector('.date').textContent = newDate;
+
 document.querySelector('.nameText').textContent = weatherData["name"];
 document.querySelector('.temp').textContent = weatherData["main"]["temp"] +'\xB0';
 document.querySelector('.weatherPage').style.background = background;
 document.querySelector('.weatherPage').style.backgroundPosition = "center";
-})
-.catch( error  => console.log('ERROR'))
+}
 
+
+button.addEventListener('click',function(){
+
+city = document.querySelector('.city');
+ state = document.querySelector('.state');
+ url = 'https://api.openweathermap.org/data/2.5/weather?q='+city.value+','+state.value+'&units=imperial&appid=1997240d660c8ec4c84f3c60d31c6079';
+
+var nameText = document.querySelector('.nameText');
+tog();
+
+fetch(url)
+.then(res => {
+	if(res.ok){
+		console.log("SUCCESS!");
+		return res.json()
+	}else{
+		console.log("NO SUCCESS");
+		document.querySelector(".nameText").textContent = "Sorry we couldn't find that city try again.";
+		reset();
+	}
 })
+.then(data =>{
+	weatherMe(data);
+	console.log("weather me");
+})
+//.catch( error  => console.log('ERROR'))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
